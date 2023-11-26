5ca389b724dc667cd8199d97426f8ea6d5b92a2a
nyafologus <nyafologus@gmail.com>
2023-11-27 00:49:07 +0100

    Add wink font to fancy text, add swooshed effect to letter H

--- src/app/ui/hero.tsx
+++ src/app/ui/hero.tsx
@@ -1,4 +1,10 @@
 import Image from "next/image";
+import localFont from "next/font/local";
+
+const winkFont = localFont({
+  src: "../../../public/wink-script-regular.woff2",
+  display: "swap",
+});
 
 export default function Hero() {
   return (
@@ -7,7 +13,14 @@ export default function Hero() {
       className="relative mb-0 flex h-screen w-full items-center justify-center overflow-hidden px-0 py-16 font-normal text-gray-300 "
     >
       <h1 className="text-center text-2xl font-bold">
-        Hey there, this is <span className="fancy-text">Sophie</span>
+        <span className={`${winkFont.className} text-5xl`}>
+          <span className="swsh">H</span>
+          ey there,
+        </span>{" "}
+        this is{" "}
+        <span className={`${winkFont.className} fancy-text pr-2 text-5xl`}>
+          Sophie
+        </span>
         <br />
         <span className="mt-4 inline-block">I am a Frontend Developer</span>
       </h1>
