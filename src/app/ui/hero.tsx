f9bf437f6f5eb0a7b5af84e932dc9cfe43c5e287
nyafologus <nyafologus@gmail.com>
2023-11-27 00:26:42 +0100

    Add fancy text, and hardcoded values for now

--- src/app/ui/hero.tsx
+++ src/app/ui/hero.tsx
@@ -6,7 +6,12 @@ export default function Hero() {
       id="hero"
       className="w-full h-screen justify-center items-center overflow-hidden relative font-normal text-gray-300 mb-0 flex py-16 px-0"
     >
-      <p className="text-2xl font-bold text-center">Hero page</p>
+      <h1 className="text-2xl font-bold text-center">
+        Hey there, this is <span className="fancy-text">Sophie</span>
+        <br />
+        <span className="inline-block mt-4">I am a Frontend Developer</span>
+      </h1>
+
       <Image
         className="w-full h-full min-h-screen absolute z-[-1] object-cover"
         src="/horsegirl-r.jpg"
