41f03160258e61615c7d7e342a95b0b520b19d25
nyafologus <nyafologus@gmail.com>
2023-11-28 05:35:13 +0100

    Add data file

---
+++ src/app/data/data.tsx
@@ -0,0 +1,85 @@
+// ABOUT
+export const aboutData = {
+  img: "avatar.png",
+  paragraphOne:
+    "Working as a contractor for a Perth-based digital agency for the past couple of years in collaboration with their talented team to build beautiful custom designed websites for their clients",
+  paragraphTwo: "✨",
+  paragraphThree: "Confident in the following technologies:",
+  paragraphFour: "✨",
+  paragraphFive:
+    "HTML5, CSS3, SASS, Flexbox, Grid, JavaScript, React.js, Gatsby.js, GraphQL, and Wordpress CMS",
+  paragraphSix: "✨",
+  paragraphSeven:
+    "Recently been working with Next.js 14, TypeScript, Tailwind, and Framer Motion",
+  resume:
+    "https://drive.google.com/file/d/14Uwm0QkneXl7an-G19utjjUYOaeQWsWY/view?usp=sharing",
+};
+
+// PROJECTS
+export const projectsData = [
+  {
+    id: crypto.randomUUID(),
+    img: "corps-transparent.png",
+    title: "Corps Group",
+    info: "This is a custom website built for an Australian construction company to help them to refresh their brand identity and showcase their business expertise.",
+    info2: "",
+    url: "https://corps.com.au/",
+  },
+  {
+    id: crypto.randomUUID(),
+    img: "rinnorris-transparent.png",
+    title: "Rin Norris",
+    info: "This site features a reputable artist who specializes in print, paint, collage and photography. The aim was to develop a small and user-friendly site to showcase her works, highlight her experience and allow users to become familiar with her background.",
+    info2: "",
+    url: "https://www.maritenorris.com/",
+  },
+  {
+    id: crypto.randomUUID(),
+    img: "mms-transparent.png",
+    title: "Map My Say",
+    info: "Map My Say is an interactive, map-based survey tool that makes it easy for the public to provide feedback to inform future planning in their community. This website aims to capture their modern, forward-thinking and friendly new brand identity, and the collaborative nature of their app.",
+    info2: "",
+    url: "https://www.mapmysay.com/",
+  },
+  {
+    id: crypto.randomUUID(),
+    img: "prm-transparent.png",
+    title: "Pilbara Rail Maintenance",
+    info: "This site was built for one of the largest privately-owned rail companies in Western Australia to  demonstrate their skills and expertise in the rail industry.",
+    info2: "",
+    url: "https://www.pilbara-rm.com.au/",
+  },
+];
+
+// CONTACT
+export const contactData = {
+  cta: "I am always happy to chat about coding projects!",
+  btn: "Check out my Github",
+  site: "https://github.com/nyafologus",
+};
+
+// FOOTER
+export const footerData = {
+  networks: [
+    {
+      id: crypto.randomUUID(),
+      name: "twitter",
+      url: "https://twitter.com/szofinauta",
+    },
+    {
+      id: crypto.randomUUID(),
+      name: "spotify",
+      url: "https://open.spotify.com/user/szofinauta",
+    },
+    {
+      id: crypto.randomUUID(),
+      name: "linkedin",
+      url: "https://www.linkedin.com/in/szof-balogh/",
+    },
+    {
+      id: crypto.randomUUID(),
+      name: "github",
+      url: "https://github.com/nyafologus",
+    },
+  ],
+};
