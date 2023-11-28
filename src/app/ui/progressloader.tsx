cee84588caa1f7c5ff17da984f5cd02f0004d5ea
nyafologus <nyafologus@gmail.com>
2023-11-28 07:23:04 +0100

    Fix progress loader - re-add className I mistakenly identified as unnecessary

--- src/app/ui/progressloader.tsx
+++ src/app/ui/progressloader.tsx
@@ -47,7 +47,12 @@ const ProgressLoader: React.FC<ProgressLoaderProps> = ({
       className={`progress-wrap ${isProgressActive ? "active-progress" : ""}`}
       onClick={handleScrollToTop}
     >
-      <svg width="100%" height="100%" viewBox="-1 -1 102 102">
+      <svg
+        className="progress-circle svg-content"
+        width="100%"
+        height="100%"
+        viewBox="-1 -1 102 102"
+      >
         <path
           ref={progressPathRef}
           d="M50,1 a49,49 0 0,1 0,98 a49,49 0 0,1 0,-98"
