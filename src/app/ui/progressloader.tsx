3a419b9c50854c55e1a3bbfffbd141980c11450a
nyafologus <nyafologus@gmail.com>
2023-11-28 05:15:53 +0100

    Add hero image for smaller viewports, remove unnecessary classname

--- src/app/ui/progressloader.tsx
+++ src/app/ui/progressloader.tsx
@@ -47,12 +47,7 @@ const ProgressLoader: React.FC<ProgressLoaderProps> = ({
       className={`progress-wrap ${isProgressActive ? "active-progress" : ""}`}
       onClick={handleScrollToTop}
     >
-      <svg
-        className="progress-circle svg-content"
-        width="100%"
-        height="100%"
-        viewBox="-1 -1 102 102"
-      >
+      <svg width="100%" height="100%" viewBox="-1 -1 102 102">
         <path
           ref={progressPathRef}
           d="M50,1 a49,49 0 0,1 0,98 a49,49 0 0,1 0,-98"
